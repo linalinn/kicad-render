bb507230486016fa6ae58dfb480c46607875b724
LinaLinn <lina.cloud@outlook.de>
2025-03-23 01:31:48 +0100

    git add rund pre render file test first

--- .github/tests/test pcb/kicad_render_pre_render.sh
+++ .github/tests/test pcb/kicad_render_pre_render.sh
@@ -3,13 +3,14 @@ echo "#"
 echo "# HI RUNNING PRE RENDERING SCRIPT"
 echo "#"
 
+ls -al $(dirname "$INPUT_PCB_FILE")
+
 echo "# INSTALLING MS FONTS"
 echo "#"
 echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections
 apt-get install ttf-mscorefonts-installer
 
-KICAD_CLI_OPTIONAL_ARGS="$KICAD_CLI_OPTIONAL_ARGS -p transparent"
-KICAD_CLI_OPTIONAL_ARGS="$KICAD_CLI_OPTIONAL_ARGS -z 2 transparent"
+KICAD_CLI_OPTIONAL_ARGS="$KICAD_CLI_OPTIONAL_ARGS -zoom 2"
 
-export KICAD_CLI_OPTIONAL_ARGS="$KICAD_CLI_OPTIONAL_ARGS -b transparent"
+export KICAD_CLI_OPTIONAL_ARGS="$KICAD_CLI_OPTIONAL_ARGS"
 export INPUT_PREFIX="prefix-via-pre-render"
