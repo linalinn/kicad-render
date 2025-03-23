c8386aa08f6af45ee105c09c252cb5fc20ce35f6
LinaLinn <lina.cloud@outlook.de>
2025-03-23 21:51:40 +0100

    add space to prefix

--- .github/tests/test pcb/kicad_render_pre_render.sh
+++ .github/tests/test pcb/kicad_render_pre_render.sh
@@ -3,8 +3,6 @@ echo "#"
 echo "# HI RUNNING PRE RENDERING SCRIPT"
 echo "#"
 
-ls -al $(dirname "$INPUT_PCB_FILE")
-
 echo "# INSTALLING MS FONTS"
 echo "#"
 apt-get update --yes 
@@ -14,4 +12,4 @@ apt-get install --yes ttf-mscorefonts-installer
 KICAD_CLI_OPTIONAL_ARGS="$KICAD_CLI_OPTIONAL_ARGS -zoom 2"
 
 export KICAD_CLI_OPTIONAL_ARGS="$KICAD_CLI_OPTIONAL_ARGS"
-export INPUT_PREFIX="prefix-via-pre-render"
+export INPUT_PREFIX="prefix-via pre-render"
