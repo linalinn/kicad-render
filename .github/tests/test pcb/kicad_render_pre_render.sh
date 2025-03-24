e3323d9f21688413ca5dc056502d7698bf0e9d73
LinaLinn <lina.cloud@outlook.de>
2025-03-24 23:12:15 +0100

    fix zoom argument

--- .github/tests/test pcb/kicad_render_pre_render.sh
+++ .github/tests/test pcb/kicad_render_pre_render.sh
@@ -9,7 +9,7 @@ apt-get update --yes
 echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections
 apt-get install --yes ttf-mscorefonts-installer
 
-KICAD_CLI_OPTIONAL_ARGS="$KICAD_CLI_OPTIONAL_ARGS -zoom 2"
+KICAD_CLI_OPTIONAL_ARGS="$KICAD_CLI_OPTIONAL_ARGS --zoom 2"
 
 export KICAD_CLI_OPTIONAL_ARGS="$KICAD_CLI_OPTIONAL_ARGS"
 export INPUT_PREFIX="prefix-via pre-render"
